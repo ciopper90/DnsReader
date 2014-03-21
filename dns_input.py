9776c820247aacd6d162b3e52ffbd71060925a1c
santomarco <santomarco10@gmail.com>
2014-03-21 07:50:19 +0100

    filtro ( forse) fatto e mantenuto quotato

--- dns_input.py
+++ dns_input.py
@@ -113,9 +113,23 @@ def main() :
     for dns_selez in dns_univ:
         app=app+' and not (src host '+ dns_selez + ' and not dst net '+ sottorete_univ+' ) and not (dst host '+ dns_selez + ' and not src net '+ sottorete_univ+' ) '
 
-        ## variante... prova e non definitiva
-        #app=app+' or ( (src host '+ dns_selez + ' and dst net '+ sottorete_univ+' ) or (dst host '+ dns_selez + ' and src net '+ sottorete_univ+' )) '
 
+    ###################################################################
+    # for dns_selez in dns_univ:
+    #      if dns_selez != dns_univ[0]:
+    #             app= app + " or "
+    #      else:
+    #             app=" and ( "
+    #      app=app+' not (src host '+ dns_selez + ' and not dst net '+ sottorete_univ+' ) and not (dst host '+ dns_selez + ' and not src net '+ sottorete_univ+' ) '
+    #
+    #      ## variante... prova e non definitiva
+    #      #app=app+' ( (src host '+ dns_selez + ' and dst net '+ sottorete_univ+' ) or (dst host '+ dns_selez + ' and src net '+ sottorete_univ+' )) '
+    #
+    # app=app+" )"
+    # predicato_di_filtro='port '+ str(port)+app#+' and (net '+sottorete_univ+' )'
+    #
+    #
+    ####################################################################
 
     predicato_di_filtro='port '+ str(port)+app#+' and (net '+sottorete_univ+' )'
     #print predicato_di_filtro
